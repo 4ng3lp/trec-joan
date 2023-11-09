fdb4226152f634d39712126a603621bf7df620f8
Àngel <aparra1970@gmail.com>
2023-11-09 19:07:35 +0100

    Create trec-avis.ps1

---
+++ trec-avis.ps1
@@ -0,0 +1,2 @@
+Add-Type -AssemblyName PresentationCore,PresentationFramework;
+[System.Windows.Forms.MessageBox]::Show("Hola, aquest missatge és part del treball de recerca de Joan, torna l'usb a consejeria. Gràcies per participar-hi!!!","Trec J04n");
