d36d3184357276b784dd7d2a745f307a8c1cb0c2
Àngel <aparra1970@gmail.com>
2023-11-09 20:16:34 +0100

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -1,2 +1,2 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework;
-[System.Windows.MessageBox]::Show("Hola, aquest missatge \u00E9s part del treball de recerca de Joan, torna l'usb a consejeria. Gr\u00E0cies per participar-hi!!!","Trec J04n");
+[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan, torna l'usb a consejeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
