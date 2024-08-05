65afac2aec31eea4df7f1e029289ca231b93a7ad
Àngel <aparra1970@gmail.com>
2024-08-06 01:06:03 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -4,7 +4,7 @@ $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
 $synth.Speak("Hoooolaaaa $env:USERNAME, A ver, alma de cantaro. ¿Como se te ocurre meter un usb? ¡Que eres muy tonto!")
 
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
-
+ 
 Function Set-ScreenResolutionAndOrientation { 
 
 <# 
