11da8b761120289f7e3d2db4a6afb7a4f4799f88
Àngel <aparra1970@gmail.com>
2024-08-06 00:05:59 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework;
 Add-Type -AssemblyName System.Speech
 $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-$synth.Speak("Hey $env:USERNAME, you are hacked!")
+$synth.Speak("Hey $env:USERNAME, estas jaqueado!")
 
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
