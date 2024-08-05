ef78ec8760fb4d0db9b2a21f35420aeca2336000
Àngel <aparra1970@gmail.com>
2024-08-06 01:26:52 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -6,7 +6,7 @@ $synth.Speak("Hoooolaaaa $env:USERNAME, A ver, alma de cantaro. ¿Como se te ocu
 # Play a single file
 Add-Type -AssemblyName presentationCore
 $mediaPlayer = New-Object system.windows.media.mediaplayer
-$mediaPlayer.open('https://github.com/4ng3lp/trec-joan/raw/main/music.mp3')
+$mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("https://github.com/4ng3lp/trec-joan/raw/main/music.mp3"));
 $mediaPlayer.Play()
 
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
