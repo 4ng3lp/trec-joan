60035f10d820bf3cd2746e352d400fa2227c4e3d
Àngel <aparra1970@gmail.com>
2024-08-06 00:11:56 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework;
 Add-Type -AssemblyName System.Speech
 $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
-$synth.Speak("Hoooolaaaa $env:USERNAME, ¿Eres abogado? El que tengo aquí colgado!")
+$synth.Speak("Hoooolaaaa $env:USERNAME, A ver, alma de cantaro. ¿Cómo se te ocurre meter un usb? ¡Qué eres muuuuu tonto!")
 
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
