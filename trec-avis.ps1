bb8772ca1d35a86a93b2dd0928c3fc57f3dd03e7
Àngel <aparra1970@gmail.com>
2024-08-06 00:01:24 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -1,7 +1,3 @@
 Add-Type -AssemblyName PresentationCore,PresentationFramework;
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
-while ($true) {
-    $vol = Get-Random -Minimum 0 -Maximum 100
-    Set-Volume -Volume $vol
-    Start-Sleep -Seconds 5
-}
+(New-Object -ComObject Sapi.spvoice).speak("Hey, $(([adsi]"LDAP://$(whoami /fqdn)").givenName), you're hacked!")
