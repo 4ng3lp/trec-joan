cc9b62d0ae2c8eff8188a6c05b5a6d4890157128
Àngel <aparra1970@gmail.com>
2024-08-06 00:20:05 +0200

    Update trec-avis.ps1

--- trec-avis.ps1
+++ trec-avis.ps1
@@ -3,4 +3,8 @@ Add-Type -AssemblyName System.Speech
 $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
 $synth.Speak("Hoooolaaaa $env:USERNAME, A ver, alma de cantaro. ¿Como se te ocurre meter un usb? ¡Que eres muuuuu tonto!")
 
+#$wshell = New-Object -ComObject wscript.shell;
+#$wshell.SendKeys('a')
+
+(New-Object -ComObject WScript.Shell).SendKeys('^{DOWN}')
 #[System.Windows.MessageBox]::Show("Hola, aquest missatge $([char]0x00E9)s part del treball de recerca de Joan. Si us plau, torna l'USB a consergeria. Gr$([char]0x00E0)cies per participar-hi!!!","Trec J04n");
